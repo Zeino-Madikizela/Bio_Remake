4af9e80429fd1da8f2a6386396ebd74daed1aa57
Zeino Madikizela <81881472+Zeino-Madikizela@users.noreply.github.com>
2022-04-04 23:20:03 +0200

    MainActivity.java
    // Hide Keyboard functionality
    
    in the method addHobbies, after clicking the button "DONE", the keyboard will auto hide :  inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);

--- app/src/main/java/com/example/bioremake/MainActivity.java
+++ app/src/main/java/com/example/bioremake/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.bioremake;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.example.bioremake.databinding.ActivityMainBinding;
 
@@ -28,5 +30,9 @@ public class MainActivity extends AppCompatActivity {
     public void addHobbies(View view){
         frontBinder.hobbiesText.setText("Hobbies : " + frontBinder.enterHobby.getText().toString().trim()  );
         frontBinder.hobbiesText.setVisibility(View.VISIBLE);
+
+        // Hide keyboard
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
