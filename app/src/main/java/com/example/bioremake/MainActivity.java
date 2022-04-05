01e3731e084938939717a6f3306c3ff5082c3fb3
Zeino Madikizela <81881472+Zeino-Madikizela@users.noreply.github.com>
2022-04-05 16:27:54 +0200

    CLASS : activity_main.xml
    Added the data tag, with name bio and the type is the location of the data class
    
    Changed the name in TextView from being accessed from the Strings document(xml) to be accessed from the data object in runtime
    
    added the text in the text view for it to be accessed in runtime as well from the data class/data object
    
    CLASS : Bio.java
    
    Created the Bio class, which is the data object
    - instance variables
    + constructors
    + getters and setters
    
    CLASS : MainActivity.java
    
    imported the data class
    Created the instance of the Bio object within the class
    Set the name(Hard Coding) of the person at compile time
    Used the binding object to set the bio from the data tag in the xml file of the class, the name of the varialbe is bio (not very imformed about this one)
    
    set up the hobbies also to access the Bio object's field.
    Also modified it to interpret the input and if it's empty, do nothing

--- app/src/main/java/com/example/bioremake/MainActivity.java
+++ app/src/main/java/com/example/bioremake/MainActivity.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.example.bioremake.data.Bio;
 import com.example.bioremake.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding frontBinder;
+    private  Bio bio = new Bio();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         frontBinder = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        bio.setName("Elon Musk");
+
+        frontBinder.setBio(bio);
         frontBinder.doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +33,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addHobbies(View view){
-        frontBinder.hobbiesText.setText("Hobbies : " + frontBinder.enterHobby.getText().toString().trim()  );
+
+        String hobbiesInput = frontBinder.enterHobby.getText().toString().trim();
+
+        if(hobbiesInput.equals("")) return; //If the string is empty, do nothing
+
+        bio.setHobbies("Hobbies : " + hobbiesInput);
+
+        //frontBinder.hobbiesText.setText("Hobbies : " + frontBinder.enterHobby.getText().toString().trim()  );
         frontBinder.invalidateAll();
         frontBinder.hobbiesText.setVisibility(View.VISIBLE);
 
